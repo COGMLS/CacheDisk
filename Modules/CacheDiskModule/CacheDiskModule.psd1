16f2ead67f09ac1d26f2ad33c7753d564ae0ad82
COGMLS <mlsmatheus@hotmail.com>
2024-01-20 13:31:52 -0300

    Added first PowerShell module for Cache Disk tools

---
+++ Modules/CacheDiskModule/CacheDiskModule.psd1
@@ -0,0 +1,14 @@
+@{
+    GUID="4b4fe32c-1c3c-4a4d-aad0-3136d4265a5e"
+    Author="Matheus L. Silvati"
+    CompanyName=""
+    Copyright="© Matheus L. Silvati. All rights reserved."
+    Description='PowerShell Module to work with Cache Disk tools'
+    ModuleVersion="0.0.1"
+    PowerShellVersion="7.0"
+    FunctionsToExport = @()
+    CmdletsToExport = @()
+    AliasesToExport = @()
+    NestedModules="CacheDiskModule.psm1"
+    HelpInfoURI = ''
+}
---
+++ Modules/CacheDiskModule/CacheDiskModule.psm1
@@ -0,0 +1,16 @@
+<######################### PowerShell Cache Disk Module #########################
+ # Project: Cache Disk
+ # --------------------------------------------------------
+ # Author: Matheus L. Silvati
+ # ------------------------------------
+ # Version: 0.0.1
+ # ------------------------------------
+ # PowerShell Minimum Version: 7.0
+ # --------------------------------------------------------
+ # OBS: N/A
+################################################################################>
+
+#Requires -Version 7.0
+
+$CacheDiskLibAsm = [System.Reflection.Assembly]::LoadFile("CacheDiskLib.dll")
+
