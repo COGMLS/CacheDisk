ef8f33010cf328db4e1db0d7780209ed3ea7ef83
COGMLS <mlsmatheus@hotmail.com>
2024-02-20 19:06:15 -0300

    Added dependencies for console. Added tests with CacheDisk lib.

--- CacheDiskConsole/Program.cs
+++ CacheDiskConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CacheDiskLib;
 
 namespace CacheDiskConsole
 {
@@ -9,7 +10,13 @@ namespace CacheDiskConsole
 			Console.WriteLine("\nCache Disk Console");
 			Console.WriteLine("---------------------------------------------------------");
 
+			CacheDisk cache = new CacheDisk("F:\\DirA", "F:\\CacheTemp");
 
+			cache.CacheItem();
+
+			cache.UnCacheItem();
+
+			Console.WriteLine("End of the test");
 		}
 	}
 }
