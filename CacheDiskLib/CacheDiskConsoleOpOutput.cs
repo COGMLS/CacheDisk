8d911a069299243beb64d8ea148cb95255b05e46
COGMLS <mlsmatheus@hotmail.com>
2024-02-21 14:58:13 -0300

    Added CacheDiskTransferTool Added CacheDiskConsoleOpOutput

---
+++ CacheDiskLib/CacheDiskConsoleOpOutput.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace CacheDiskLib
+{
+	internal static class CacheDiskConsoleOpOutput
+	{
+		internal static void WriteProgress(uint total, uint count, bool showItemsProcessed = false, long totalSize = 0, long actualSize = 0, double speed = 0)
+		{
+			double actualPercentage = ((double)count / (double)total) * 100;
+
+			int c = Console.GetCursorPosition().Left;
+			int r = Console.GetCursorPosition().Top;
+			int bufferWidth = Console.BufferWidth;
+
+			bufferWidth /= 2;
+
+			int PercentagePerChar = (int)((uint)bufferWidth * actualPercentage / 100);
+
+			Console.SetCursorPosition(0, r);
+
+			for (int i = 0; i < bufferWidth && i < PercentagePerChar; i++)
+			{
+				//\u2588
+				Console.Write('\u25a0');
+			}
+
+			Console.SetCursorPosition(bufferWidth + 1, r);
+
+			Console.Write($" {actualPercentage.ToString("N1")}%");
+
+			if (showItemsProcessed)
+			{
+				Console.Write($" ({count} / {total})");
+
+				if (totalSize > 0 && actualSize >= 0)
+				{
+					double totalSizeF = totalSize;
+					double actualSizeF = actualSize;
+
+					string unitTotalSize = "B";
+
+					if (totalSize >= 1000 && totalSize < 1_000_000)
+					{
+						unitTotalSize = "KB";
+						totalSizeF = totalSize / 1000;
+					}
+					else if (totalSize >= 1_000_000 && totalSize < 1_000_000_000)
+					{
+						unitTotalSize = "MB";
+						totalSizeF = totalSize / 1_000_000;
+					}
+					else if (totalSize >= 1_000_000_000 && totalSize < 1_000_000_000_000)
+					{
+						unitTotalSize = "GB";
+						totalSizeF = totalSize / 1_000_000_000;
+					}
+					else if (totalSize >= 1_000_000_000_000)
+					{
+						unitTotalSize = "TB";
+						totalSizeF = totalSize / 1_000_000_000_000;
+					}
+
+					string unitActualSize = "B";
+
+					if (actualSize >= 1000 && actualSize < 1_000_000)
+					{
+						unitActualSize = "KB";
+						actualSizeF = actualSize / 1000;
+					}
+					else if (actualSize >= 1_000_000 && actualSize < 1_000_000_000)
+					{
+						unitActualSize = "MB";
+						actualSizeF = actualSize / 1_000_000;
+					}
+					else if (actualSize >= 1_000_000_000 && actualSize < 1_000_000_000_000)
+					{
+						unitActualSize = "GB";
+						actualSizeF = actualSize / 1_000_000_000;
+					}
+					else if (actualSize >= 1_000_000_000_000)
+					{
+						unitActualSize = "TB";
+						actualSizeF = actualSize / 1_000_000_000_000;
+					}
+
+					Console.Write($" ({actualSizeF.ToString("N1")}{unitActualSize} / {totalSizeF.ToString("N1")}{unitTotalSize})");
+				}
+
+				if (speed > 0)
+				{
+					string unit = "B";
+
+					if (speed >= 1000 && speed < 1_000_000)
+					{
+						unit = "KB";
+						speed /= 1000;
+					}
+					else if (speed >= 1_000_000 && speed < 1_000_000_000)
+					{
+						unit = "MB";
+						speed /= 1_000_000;
+					}
+					else if (speed >= 1_000_000_000 && speed < 1_000_000_000_000)
+					{
+						unit = "GB";
+						speed /= 1_000_000_000;
+					}
+					else
+					{
+						unit = "TB";
+						speed /= 1_000_000_000_000;
+					}
+
+					Console.Write($" ({speed.ToString("N1")} {unit}/s)");
+				}
+			}
+
+			c = Console.GetCursorPosition().Left + 1;
+
+			for (; c < Console.BufferWidth; c++)
+			{
+				Console.Write(' ');
+			}
+
+			if (actualPercentage == 100)
+			{
+				Console.WriteLine();
+			}
+		}
+	}
+}
---
+++ CacheDiskLib/CacheDiskTransferTool.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security;
+using System.Security.AccessControl;
+
+namespace CacheDiskLib
+{
+	internal static class CacheDiskTransferTool
+	{
+		public static void TransferData (string path, string destination, CacheType operationType, bool hiddenDestination, bool replicateAttributes, bool replicateAccessControl, ref List<Exception> ErrorList, bool showOnConsole = false)
+		{
+			DirectoryInfo PathInfo = new DirectoryInfo(path);
+			DirectoryInfo DestinationInfo = new DirectoryInfo(destination);
+
+			IEnumerable<DirectoryInfo> pathDirs = PathInfo.EnumerateDirectories("*", SearchOption.AllDirectories);
+			IEnumerable<FileInfo> pathFiles = PathInfo.EnumerateFiles("*", SearchOption.AllDirectories);
+
+			uint totalDirs = (uint)pathDirs.Count();
+			uint totalFiles = (uint)pathFiles.Count();
+			
+			uint totalItems = totalDirs + totalFiles;
+			uint actualProcessed = 0;
+			
+			// Set the Access Control for destination directory:
+			if (replicateAccessControl)
+			{
+				if (OperatingSystem.IsWindows())
+				{
+					try
+					{
+						DirectorySecurity pathAccessControle = PathInfo.GetAccessControl();
+						DestinationInfo.SetAccessControl(pathAccessControle);
+					}
+					catch (Exception e)
+					{
+						ErrorList.Add(e);
+					}
+				}
+			}
+
+			if (!DestinationInfo.Exists)
+			{
+				DestinationInfo.Create();
+			}
+
+			if (showOnConsole)
+			{
+				CacheDiskConsoleOpOutput.WriteProgress(totalItems, actualProcessed, true);
+			}
+
+			// Replicate the directories:
+			foreach (DirectoryInfo pathDir in pathDirs)
+			{
+				try
+				{
+					string destTmp = pathDir.FullName.Replace(path, destination);
+
+					if (!Directory.Exists(destTmp))
+					{
+						DirectoryInfo destInfoTmp = Directory.CreateDirectory(destTmp);
+
+						if (destInfoTmp.Exists)
+						{
+							actualProcessed++;
+
+							// On Windows, try to set the Security Access Control:
+							if (replicateAccessControl)
+							{
+								if (OperatingSystem.IsWindows())
+								{
+									try
+									{
+										DirectorySecurity pathDirSecurity = pathDir.GetAccessControl(System.Security.AccessControl.AccessControlSections.All);
+										destInfoTmp.SetAccessControl(pathDirSecurity);
+									}
+									catch (Exception e)
+									{
+										ErrorList.Add(e);
+									}
+								}
+							}
+
+							// Set the Directory Attributes on Windows OS and UnixMode on Unix systems:
+							if (replicateAttributes)
+							{
+								try
+								{
+									if (OperatingSystem.IsWindows())
+									{
+										FileAttributes pathDirAttr = pathDir.Attributes;
+										destInfoTmp.Attributes = pathDirAttr;
+									}
+									else
+									{
+										UnixFileMode pathDirUnixMode = pathDir.UnixFileMode;
+										destInfoTmp.UnixFileMode = pathDirUnixMode;
+									}
+								}
+								catch (Exception e)
+								{
+									ErrorList.Add(e);
+								}
+							}
+						}
+						else
+						{
+							ErrorList.Add(new Exception($"Fail to create directory ({destInfoTmp.FullName})"));
+						}
+					}
+				}
+				catch (Exception e)
+				{
+					ErrorList.Add(e);
+				}
+
+				if (showOnConsole)
+				{
+					CacheDiskConsoleOpOutput.WriteProgress(totalItems, actualProcessed, true);
+				}
+			}
+
+			// Copy the files:
+			foreach (FileInfo pathFile in pathFiles)
+			{
+				try
+				{
+					string destFileTmp = pathFile.FullName.Replace(path, destination);
+
+					if (!File.Exists(destFileTmp))
+					{
+						FileInfo destFileInfoTmp = pathFile.CopyTo(destFileTmp);
+
+						// Verify if the copy exist:
+						if (destFileInfoTmp.Exists)
+						{
+							actualProcessed++;
+
+							// On Windows try to apply the Security Access Control:
+							if (replicateAccessControl)
+							{
+								if (OperatingSystem.IsWindows())
+								{
+									try
+									{
+										FileSecurity pathFileSecurity = pathFile.GetAccessControl(AccessControlSections.All);
+										destFileInfoTmp.SetAccessControl(pathFileSecurity);
+									}
+									catch (Exception e)
+									{
+										ErrorList.Add(e);
+									}
+								}
+							}
+
+							// Set the file attributes on Windows and on Unix systems set the Unix Mode:
+							if (replicateAttributes)
+							{
+								try
+								{
+									if (OperatingSystem.IsWindows())
+									{
+										FileAttributes pathFileAttr = pathFile.Attributes;
+										destFileInfoTmp.Attributes = pathFileAttr;
+									}
+									else
+									{
+										UnixFileMode pathFileUnixMode = pathFile.UnixFileMode;
+										destFileInfoTmp.UnixFileMode = pathFileUnixMode;
+									}
+								}
+								catch (Exception e)
+								{
+									ErrorList.Add(e);
+								}
+							}
+						}
+						else
+						{
+							ErrorList.Add(new Exception ($"Fail to copy ({pathFile.FullName}) to ({destFileTmp})"));
+						}
+					}
+				}
+				catch (Exception e)
+				{
+					ErrorList.Add(e);
+				}
+
+				if (showOnConsole)
+				{
+					CacheDiskConsoleOpOutput.WriteProgress(totalItems, actualProcessed, true);
+				}
+			}
+
+			// Remove the items from original path if is a MOVE operation:
+			if (operationType == CacheType.MOVE)
+			{
+				try
+				{
+					PathInfo.Delete(true);
+				}
+				catch (Exception e)
+				{
+					ErrorList.Add(e);
+				}
+			}
+
+			// Hidden the destination directory:
+			if (hiddenDestination)
+			{
+				DestinationInfo.Attributes &= FileAttributes.Hidden;
+			}
+		}
+	}
+}
