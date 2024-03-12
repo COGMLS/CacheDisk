8ed86afbe213b115c3750b321ed89bb5ca8ec61c
COGMLS <mlsmatheus@hotmail.com>
2024-03-12 15:04:59 -0300

    Add new tests to console.

--- CacheDiskConsole/Program.cs
+++ CacheDiskConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using CacheDiskLib;
 
 namespace CacheDiskConsole
@@ -10,11 +11,43 @@ namespace CacheDiskConsole
 			Console.WriteLine("\nCache Disk Console");
 			Console.WriteLine("---------------------------------------------------------");
 
-			CacheDisk cache = new CacheDisk("F:\\DirA", "F:\\CacheTemp");
+#if DEBUG
+			string path = "";
+			string destination = "";
+			string backup = "";
+			bool useBackup = false;
+
+			if (args.Length > 0)
+			{
+				path = args[0];
+				destination = args[1];
+			}
+
+			if (args.Length > 2)
+			{
+				backup = args[2];
+				useBackup = true;
+			}
+
+			//Tests.OpenDirectories(path, destination);
+
+			CacheDisk cache;
+
+			if (useBackup)
+			{
+				cache = new CacheDisk(path, destination, backup);
+			}
+			else
+			{
+				cache = new CacheDisk(path, destination);
+			}
+
+			cache.SetConsoleOutputOperation(true);
 
 			cache.CacheItem();
 
-			cache.UnCacheItem();
+			cache.RestoreCache();
+#endif // DEBUG
 
 			Console.WriteLine("End of the test");
 		}
